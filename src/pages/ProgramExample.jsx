2d8e86041beaf0b8723bf78fabbfbd9a8a10da8f
Amalia Dewii <amaliaadk26@gmail.com>
2024-10-25 11:19:40 +0700

    update table materials component

--- src/pages/ProgramExample.jsx
+++ src/pages/ProgramExample.jsx
@@ -5,7 +5,7 @@ import data from "../data/program";
 // pemanggilan
 export default function ProgramExample() {
   return (
-    <div className="bg-[#F1FAFF]">
+    <div className="bg-[#F1FAFF] container">
       {data.map(function (item, index) {
         return (
           <div key={index} className="grid grid-cols-2 pt-10">
@@ -47,14 +47,13 @@ function CardClass({
       </div>
 
       <p className="text-start mb-4 mx-5">{description}</p>
-      <ul className="ml-5">
+      <ul className="ml-5 space-y-2">
         {benefits
           ? benefits.map(function (benefit, index) {
               return (
-                <li key={index}>
-                  {benefit}
-                  {/* blm sejajar */}
+                <li key={index} className="flex items-center space-x-2">
                   <img src="check1.svg" alt="icons" />
+                  <span>{benefit}</span>
                 </li>
               );
             })
@@ -70,18 +69,18 @@ function CardClass({
 // desain table
 function TableMaterial({ meetings }) {
   return (
-    <table>
-      <tr>
-        <th className="border border-black">pertemuan ke</th>
-        <th className="border border-black">materi</th>
+    <table className="w-10/12">
+      <tr className="bg-[#1E2538] text-center text-white ">
+        <th className="border border-blue-950 py-3">THE TH MEETING</th>
+        <th className="border border-blue-950">MATERIALS</th>
       </tr>
 
       {meetings
         ? meetings.map(function (meeting, index) {
             return (
-              <tr key={index}>
-                <td className="border border-black">{meeting.pertemuan}</td>
-                <td className="border border-black">{meeting.materials}</td>
+              <tr key={index} className={index % 2 === 0 ? "bg-[#F1FAFF" : "bg-[#7D8DA6]"}>
+                <td className="border border-blue-950 text-center py-1">{meeting.pertemuan}</td>
+                <td className="border border-blue-950 text-center py-1">{meeting.materials}</td>
               </tr>
             );
           })
@@ -89,3 +88,5 @@ function TableMaterial({ meetings }) {
     </table>
   );
 }
+
+// nyamain tabelnya
